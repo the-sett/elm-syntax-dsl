780e48f30d142d0bfebed7cd49a1d9c82b224e91
Rupert Smith <rupert@thesett.com>
2019-08-22 14:53:46 +0100

    Starting work on the pretty printer.

---
+++ src/Elm/Pretty.elm
@@ -0,0 +1,22 @@
+module Elm.Pretty exposing (pretty)
+
+import Elm.Syntax.Declaration exposing (Declaration(..))
+import Elm.Syntax.Exposing exposing (ExposedType, Exposing(..), TopLevelExpose(..))
+import Elm.Syntax.Expression exposing (Case, CaseBlock, Expression(..), Function, FunctionImplementation, Lambda, LetBlock, LetDeclaration(..), RecordSetter)
+import Elm.Syntax.File exposing (File)
+import Elm.Syntax.Import exposing (Import)
+import Elm.Syntax.Infix exposing (Infix, InfixDirection(..))
+import Elm.Syntax.Module exposing (DefaultModuleData, EffectModuleData, Module(..))
+import Elm.Syntax.ModuleName exposing (ModuleName)
+import Elm.Syntax.Node exposing (Node(..))
+import Elm.Syntax.Pattern exposing (Pattern(..), QualifiedNameRef)
+import Elm.Syntax.Range exposing (Location, Range, emptyRange)
+import Elm.Syntax.Signature exposing (Signature)
+import Elm.Syntax.Type exposing (Type, ValueConstructor)
+import Elm.Syntax.TypeAlias exposing (TypeAlias)
+import Elm.Syntax.TypeAnnotation exposing (RecordDefinition, RecordField, TypeAnnotation(..))
+
+
+pretty : File -> String
+pretty file =
+    ""
