1fe9279a63ebd3a755684d8ce4bd802f7cca2743
Rupert Smith <rupert@thesett.com>
2019-08-22 17:21:00 +0100

    Work on the pretty printer.

--- src/Elm/Pretty.elm
+++ src/Elm/Pretty.elm
@@ -8,15 +8,42 @@ import Elm.Syntax.Import exposing (Import)
 import Elm.Syntax.Infix exposing (Infix, InfixDirection(..))
 import Elm.Syntax.Module exposing (DefaultModuleData, EffectModuleData, Module(..))
 import Elm.Syntax.ModuleName exposing (ModuleName)
-import Elm.Syntax.Node exposing (Node(..))
+import Elm.Syntax.Node as Node exposing (Node(..))
 import Elm.Syntax.Pattern exposing (Pattern(..), QualifiedNameRef)
 import Elm.Syntax.Range exposing (Location, Range, emptyRange)
 import Elm.Syntax.Signature exposing (Signature)
 import Elm.Syntax.Type exposing (Type, ValueConstructor)
 import Elm.Syntax.TypeAlias exposing (TypeAlias)
 import Elm.Syntax.TypeAnnotation exposing (RecordDefinition, RecordField, TypeAnnotation(..))
+import Pretty exposing (Doc)
 
 
-pretty : File -> String
+deNode =
+    Node.value
+
+
+deNodeAll =
+    List.map deNode
+
+
+pretty : File -> Doc
 pretty file =
-    ""
+    Pretty.lines
+        [ prettyModule (deNode file.moduleDefinition)
+        , prettyComments (deNodeAll file.comments)
+        , prettyImports (deNodeAll file.imports)
+        , prettyDeclarations (deNodeAll file.declarations)
+        ]
+
+
+prettyModule : Module -> Doc
+prettyModule mod =
+    case mod of
+        NormalModule defaultModuleData ->
+            prettyDefaultModuleData defaultModuleData
+
+        PortModule defaultModuleData ->
+            prettyDefaultModuleData defaultModuleData
+
+        EffectModule effectModuleData ->
+            prettyEffectModuleData effectModuleData
