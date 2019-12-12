d09e31e5ff828ef0cf7476043e26b701c9b8ced3
Rupert Smith <rupert@thesett.com>
2019-12-12 15:40:20 +0000

    Improving module documentation.

--- src/Elm/Comments.elm
+++ src/Elm/Comments.elm
@@ -1,10 +1,29 @@
-module Elm.Comments exposing (Comment(..), CommentPart(..), DocComment, FileComment, prettyDocComment, prettyFileComment)
+module Elm.Comments exposing
+    ( Comment(..), CommentPart(..), DocComment, FileComment
+    , prettyDocComment, prettyFileComment
+    , docCommentParser, fileCommentParser
+    )
 
 {-| A component DSL that helps with building comments.
 
 It is useful to have this in a structured way, so that it can be re-flowed by
 the pretty printer, and so that an understanding of the layout of the doc tags
 can be extracted to order the exposing clause by.
+
+
+# Structured comments
+
+@docs Comment, CommentPart, DocComment, FileComment
+
+
+# Pretty printing of comments
+
+@docs prettyDocComment, prettyFileComment
+
+
+# Parsing of comments into structured comments
+
+@docs docCommentParser, fileCommentParser
 
 -}
 
