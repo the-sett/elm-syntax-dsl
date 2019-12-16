738b31ed605631351f6ebb21a97350bf598e3437
Rupert Smith <rupert@thesett.com>
2019-12-16 13:10:14 +0000

    Passing through all comments as markdown strings.

--- src/Elm/DSLParser.elm
+++ src/Elm/DSLParser.elm
@@ -12,12 +12,14 @@ determine how to lay out exposing lists to match.
 
 -}
 
-import Elm.CodeGen exposing (File)
+import Elm.CodeGen as CG exposing (Declaration, File)
 import Elm.Parser
 import Elm.Processing
+import Elm.Syntax.Declaration as ESD
+import Elm.Syntax.Expression exposing (Function)
 import Elm.Syntax.File
 import Parser exposing (DeadEnd, Parser)
-import Util exposing (denode)
+import Util exposing (denode, denodeAll, denodeMaybe, nodify)
 
 
 parse : String -> Result (List DeadEnd) File
@@ -29,25 +31,89 @@ parse val =
 
 parseFileComments : Elm.Syntax.File.File -> File
 parseFileComments file =
-    case List.reverse file.comments |> List.head of
+    let
+        fileComments =
+            case List.reverse file.comments |> List.head of
+                Nothing ->
+                    Nothing
+
+                Just fc ->
+                    CG.emptyFileComment |> CG.markdown (denode fc) |> Just
+    in
+    CG.file (denode file.moduleDefinition)
+        (denodeAll file.imports)
+        (List.map parseDeclComments (denodeAll file.declarations))
+        fileComments
+
+
+parseDeclComments : ESD.Declaration -> Declaration
+parseDeclComments decl =
+    let
+        docComments =
+            case docsFromDecl decl of
+                Nothing ->
+                    Nothing
+
+                Just val ->
+                    CG.emptyDocComment |> CG.markdown val |> Just
+    in
+    case docComments of
         Nothing ->
-            { moduleDefinition = file.moduleDefinition
-            , imports = file.imports
-            , declarations = []
-            , comments = Nothing
-            }
+            CG.DeclNoComment (declNoDocs decl)
 
-        Just fc ->
-            { moduleDefinition = file.moduleDefinition
-            , imports = file.imports
-            , declarations = []
-            , comments =
-                Just
-                    (Elm.CodeGen.emptyFileComment
-                        |> Elm.CodeGen.markdown (denode fc)
-                    )
-            }
+        Just val ->
+            CG.DeclWithComment val (declWithDocs decl)
 
 
+{-| Replaces the documentation on a declaration with the specified string.
+-}
+declWithDocs : ESD.Declaration -> String -> ESD.Declaration
+declWithDocs decl docs =
+    case decl of
+        ESD.FunctionDeclaration funDecl ->
+            ESD.FunctionDeclaration
+                { funDecl | documentation = nodify docs |> Just }
 
---(List Elm.Syntax.Declaration.Declaration -> Elm.Syntax.File.File)
+        ESD.AliasDeclaration aliasDecl ->
+            ESD.AliasDeclaration { aliasDecl | documentation = nodify docs |> Just }
+
+        ESD.CustomTypeDeclaration customTypeDecl ->
+            ESD.CustomTypeDeclaration { customTypeDecl | documentation = nodify docs |> Just }
+
+        _ ->
+            decl
+
+
+{-| Strips the docs from a declaration.
+-}
+declNoDocs : ESD.Declaration -> ESD.Declaration
+declNoDocs decl =
+    case decl of
+        ESD.FunctionDeclaration funDecl ->
+            ESD.FunctionDeclaration
+                { funDecl | documentation = Nothing }
+
+        ESD.AliasDeclaration aliasDecl ->
+            ESD.AliasDeclaration { aliasDecl | documentation = Nothing }
+
+        ESD.CustomTypeDeclaration customTypeDecl ->
+            ESD.CustomTypeDeclaration { customTypeDecl | documentation = Nothing }
+
+        _ ->
+            decl
+
+
+docsFromDecl : ESD.Declaration -> Maybe String
+docsFromDecl decl =
+    case decl of
+        ESD.FunctionDeclaration funDecl ->
+            denodeMaybe funDecl.documentation
+
+        ESD.AliasDeclaration aliasDecl ->
+            denodeMaybe aliasDecl.documentation
+
+        ESD.CustomTypeDeclaration customTypeDecl ->
+            denodeMaybe customTypeDecl.documentation
+
+        _ ->
+            Nothing
